3426760efc2dcbbe9812d5deb93bb4707308ffd6
JHONES SANTOS <jhones.santos@fatec.sp.gov.br>
2017-09-26 18:27:45 -0300

    finalizando o capitulo 2

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -1,8 +1,22 @@
+var contatos = [
+    { _id: '1', nome: 'contato ex 1', email: 'teste_email1@email.com' },
+    { _id: '2', nome: 'contato ex 2', email: 'teste_email2@email.com' },
+    { _id: '3', nome: 'contato ex 3', email: 'teste_email3@email.com' },
+    { _id: '4', nome: 'contato ex 4', email: 'teste_email4@email.com' },
+];
+
 module.exports.listaContatos = function(req, res) {
-    res.json([
-        { _id: 1, nome: 'contato ex 1', email: 'teste_email1@email.com' },
-        { _id: 2, nome: 'contato ex 2', email: 'teste_email2@email.com' },
-        { _id: 3, nome: 'contato ex 3', email: 'teste_email3@email.com' },
-        { _id: 4, nome: 'contato ex 4', email: 'teste_email4@email.com' },
-    ]);
+    res.json(contatos);
+};
+
+exports.obtemContato = function(req, res) {
+    var idContato = req.params.id;
+
+    var contato = contatos.filter(function(param) {
+        return param._id === idContato;
+    })[0];
+
+    contato ?
+        res.json(contato) :
+        res.status(404).send('contato nao encontrado');
 };
--- contatooh/app/routes/contato.js
+++ contatooh/app/routes/contato.js
@@ -1,3 +1,4 @@
 module.exports = function(app) {
     app.get('/contatos', app.controllers.contato.listaContatos);
+    app.get('/contatos/:id', app.controllers.contato.obtemContato);
 };
